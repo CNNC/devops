19d6fefb7e32b7f30eae537891e6f60fd4830878
vic <zxdzyx@qq.com>
2022-03-02 14:48:59 +0800

    更新替换

--- bejing/test/replace.ps1
+++ bejing/test/replace.ps1
@@ -1,6 +1,15 @@
 # copy-item /Users/vic/Documents/devops-cm/devops/bejing/test/ /Users/vic/Documents/devops-cm/devops/bejing/test/bak  #在替换之前先对文件进行备份
 
 $files = Get-Childitem /Users/vic/Documents/devops-cm/devops/bejing/test/ -Recurse -Include *.yaml
+$cross =zz-dd
+$crossregionid =
+$subnetId = subnet-or3xp7fd
+$loadbalanceid =lb-4lt3b1t7
+$tkeexistedlbid =lb-4lt3b1t7
+$crossvpcid=123
+
+$servicename='/Users/vic/Documents/devops-cm/devops/bejing/test/' + $cross + '-service.yaml'
+$ingressname='/Users/vic/Documents/devops-cm/devops/bejing/test/' + $cross + '-service.yaml'
 
 foreach ($file in $files)   #遍历文件
 
@@ -14,8 +23,15 @@ foreach ($file in $files)   #遍历文件
 
    {
 
-   $liner = $line.Replace("12345","apiVersion").Replace("12345","apiVersion").Replace("12345","apiVersion").Replace("12345","apiVersion").Replace("12345","apiVersion").Replace("12345","apiVersion").Replace("12345","apiVersion")
+   $liner = $line.Replace("ap-beijing","$crossregionid").Replace("subnet-or3xp7fd",$subnetId).Replace("lb-4lt3b1t7",$loadbalanceid).Replace("lb-4lt3b1t7",$tkeexistedlbid).Replace("vpc-2gzmvizm","$crossvpcid")
 
+   
+   if ($files.pspath=/Users/vic/Documents/devops-cm/devops/bejing/test/ingress.yaml) {
+    $files.pspath=$ingressname
+    else {
+        $files.pspath=$servicename
+    }
+   }
    Add-content $file.pspath -Value $liner #附件内容到原文件
 
    }
